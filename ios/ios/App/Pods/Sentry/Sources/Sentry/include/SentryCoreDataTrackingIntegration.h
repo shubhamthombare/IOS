7ff6a69dbfe7307417219c89175e486e8928ac64
Shubhamthombare <shubhamthombare0809@gmail.com>
2023-02-13 12:58:36 +0530

    Delete ios/ios directory

--- ios/ios/App/Pods/Sentry/Sources/Sentry/include/SentryCoreDataTrackingIntegration.h
+++
@@ -1,9 +0,0 @@
-#import "SentryIntegrationProtocol.h"
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface SentryCoreDataTrackingIntegration : NSObject <SentryIntegrationProtocol>
-
-@end
-
-NS_ASSUME_NONNULL_END
